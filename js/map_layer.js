a84f7a557eb2f7a1ff185571f5938f1dddd2a7bf
Tadas Subonis <tadas.subonis@gmail.com>
2016-04-11 21:21:28 +0300

    Use MapBox

--- js/map_layer.js
+++ js/map_layer.js
@@ -1,6 +1,7 @@
 techMapApp.controller('TechMapLayerController', function ($scope, $rootScope) {
-
-    var mapBoxTile = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+    var mapBoxApiKey = 'pk.eyJ1IjoidGFzdWJvIiwiYSI6ImNpazZ0NG9hdzAwNTZ4YW0xaTdheGRlZjUifQ.d6fPkeFArHt8hivdkF4vEQ';
+    
+    var mapBoxTile = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapBoxApiKey, {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -22,8 +23,8 @@ techMapApp.controller('TechMapLayerController', function ($scope, $rootScope) {
     var center = L.latLng(54.68547, 25.28739);
     var map = L.map('map', {
         center: center, zoom: 12, layers: [
-               openStreetMapTile,
-            // mapBoxTile,
+            //    openStreetMapTile,
+            mapBoxTile,
             //            cloudMatTile,
         ]
     });
